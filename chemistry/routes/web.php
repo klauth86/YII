185a94aed95db8385d49bee9f17c0bd7d0be572e
Artur Khayrullin <kehel@yandex.ru>
2020-01-16 11:59:47 +0300

    Chapter 2 Skeleton

--- chemistry/routes/web.php
+++ chemistry/routes/web.php
@@ -13,13 +13,26 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('events', 'EventsController@index'); 
+Route::view('about', 'about.index')->name('about.index');
 
-Route::get('eventswitharray/{id}', 'EventsController@showwitharray')->name('events.showwitharray');
+Route::view('about/book', 'about.book')->name('about.book');
+
+Route::view('about/faq', 'about.faq')->name('about.faq');
+
+Route::view('about/privacy', 'about.privacy')->name('about.privacy');
+
+Route::view('about/tos', 'about.tos')->name('about.tos');
+
+Route::view('contact', 'contact.index')->name('contact.index');
+
+Route::get('events', 'EventsController@index')->name('events.index');
 
 Route::get('events/{id}', 'EventsController@show')->name('events.show');
 
-Route::get(
-	'events/category/{category}/{subcategory?}',
-	'EventsController@category'
-);
+Route::get('languages', 'LanguagesController@index')->name('languages.index');
+
+Route::get('locations', 'LocationsController@index')->name('locations.index');
+
+Route::get('map', 'MapsController@index')->name('maps.index');
+
+Auth::routes();
