4c2452eb177493fbca2182abdce519ef4fe4a33d
klauth86 <kehel@yandex.ru>
2020-01-17 09:30:19 +0300

    Add reference table

---
+++ chemistry/database/migrations/2020_01_17_062208_create_account_refs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountRefsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_orm_account_refs', function (Blueprint $table) {
+            $table->string('facebook_login', 127);
+            $table->string('reference', 127);			
+			$table->boolean('is_telegram');
+			$table->boolean('is_active')->default(true);
+			$table->primary(['facebook_login', 'reference']);
+			$table->foreign('facebook_login')->references('facebook_login')->on('t_orm_account');	
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t_orm_account_refs');
+    }
+}
